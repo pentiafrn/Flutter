705cd2f7feb84bcb14e9e30f9be391ebc2cff732
jeverdrengen <jesper@vivamusic.dk>
2019-01-25 11:57:42 +0100

    add photoslist refactor wordlist to separate folder structure cleaning up main.dart file

---
+++ lib/wordList/myApp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'randomWords.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new RandomWords(),
+    );
+  }
+}
